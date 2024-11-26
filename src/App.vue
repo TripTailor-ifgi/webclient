311e6e19d7d674a084159de25e9af3adacbf7bca
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2024-11-26 12:51:20 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <b>THIS WILL BE THE INDEX/ LANDING PAGE</b>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map/index.html'),
+        quiz: resolve(__dirname, 'quiz/index.html')
+      }
+    }
+   }
+})
